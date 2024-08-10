0c8f6f6d467af32b4894b1e83bc2f055469daba6
RoseMcc04 <mccormacka@appstate.edu>
2024-08-10 19:18:15 -0400

    writing notes in Swift, not worried about file execution

---
+++ Swift/Demo.swift
@@ -0,0 +1,19 @@
+// I will practice concepts I have currently learned in Swift. 
+
+// Declaring an integer variable
+var demoVar: Int = 5
+
+// Declaring a double variable
+var doubleVar: Double = 42.69
+
+// Declaring a boolean variable
+var boolVar: Bool = true
+
+// Declaring a string variable
+var stringVar: String = "Rose McCormack"
+
+// Print statements for all of the variables
+print(demoVar) // prints 5
+print(doubleVar) // prints 42.69
+print(boolVar) // prints true
+print(stringVar) // prints Rose McCormack
