d108387d79b72bf1e06a6d748eeae489b24cf019
RoseMcc04 <mccormacka@appstate.edu>
2024-08-15 11:08:54 -0400

    created Tree() constructor and print() method for Tree objects. They use undunderlying tree nodes which connect children through Array Lists of Tree Nodes

---
+++ Java/Trees/Tree.java
@@ -0,0 +1,55 @@
+package Java.Trees;
+
+/**
+ * @author Rose McCormack
+ * @version 2024
+ */
+public class Tree 
+{
+    private TreeNode root;
+
+    /**
+     * @param root Root node of Tree object
+     * @description The no-arg constructor allows us to create a Tree object
+     * with an input of a root node. 
+     */
+    public Tree(TreeNode root) 
+    {
+        this.root = root;
+    }
+
+    /**
+     * @description The helper method that will help us with the regular print
+     * method. 
+     */
+    public void print() 
+    {
+        print(this.root, 0);
+    }
+
+    /**
+     * @param cursor Current node to start with
+     * @param level Amount of tree levels printed
+     * @description This method will print out the tree object based on the 
+     * amount of levels in the input. 
+     */
+    public void print(TreeNode cursor, int level) 
+    {
+        String levelMarks = "";
+        for (int i = 0; i < level; i++) 
+        {
+            levelMarks += "-- ";
+        }
+        System.out.println(levelMarks + cursor.getData());
+        for (TreeNode child : cursor.getChildren()) 
+        {
+            print(child, level + 1);
+        }
+    }
+
+    // Depth-First Traversal
+
+    /*
+     * 
+     */
+}
